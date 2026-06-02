0e368f9003bbe26e06ab2ef9d82f31f086118627
nico beltramo <nico.beltramo06@gmail.com>
2026-06-02 23:11:09 +0200

    codice pendolo smorzato

---
+++ pendolo_smorzato/0,3.py
@@ -0,0 +1,81 @@
+import numpy as np
+from scipy import optimize, signal
+import matplotlib.pyplot as mp
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+
+# grafico dei punti
+tempo, tetaP = np.loadtxt("./0,3.csv", delimiter=",", unpack=True, skiprows=1)
+teta = tetaP / 180 * np.pi
+fig, ax = mp.subplots()
+ax.set_xlabel("TEMPO (s)", fontsize=11)
+ax.set_ylabel("TETA (rad)", fontsize=11)
+ticklist = np.arange(0, 25, 5)
+ticklabel = ["0", "5", "10", "15", "20"]
+ax.set_xticks(ticklist)
+ax.set_xticklabels(ticklabel)
+ax.xaxis.set_minor_locator(MultipleLocator(2.5))
+ax.grid(which="major", axis="x", linewidth=1, alpha=0.7)
+ax.grid(which="minor", axis="x", linewidth=0.5, alpha=0.5)
+tempoErr = 0.1 * np.ones(len(teta))
+tetaErr = (0.18 / 180 * np.pi) * np.ones(len(teta))
+ax.axhline(0, c="k", lw=0.5)
+ax.errorbar(
+    tempo,
+    teta,
+    xerr=tempoErr,
+    yerr=tetaErr,
+    ls="-",
+    lw=0.25,
+    c="r",
+    marker="o",
+    mfc="r",
+    ms=0.5,
+    mec="r",
+)
+
+
+# calcolo linea di approssimazione
+def f(tempo, teta0, gamma, omega, phi):
+    return teta0 * np.exp(-gamma * tempo) * np.sin(omega * tempo + phi)
+
+
+parametri, covar = optimize.curve_fit(f, tempo, teta)
+print(parametri)
+fig2, ax2 = mp.subplots()
+ax2.set_title("corrente = 0.3A", fontsize=16, pad=15)
+ax2.set_xlabel("TEMPO (s)", fontsize=11)
+ax2.set_ylabel("TETA (rad)", fontsize=11)
+ax2.set_xticks(ticklist)
+ax2.set_xticklabels(ticklabel)
+ax2.xaxis.set_minor_locator(MultipleLocator(1.25))
+ax2.grid(which="major", axis="x", linewidth=1, alpha=0.7)
+ax2.grid(which="minor", axis="x", linewidth=0.5, alpha=0.5)
+ax.axhline(0, c="k", lw=0.5)
+ax2.errorbar(
+    tempo,
+    teta,
+    ls="none",
+    marker="o",
+    ms=2,
+    mfc="b",
+    mec="b",
+    label="valori sperimentali",
+)
+tempo2 = np.arange(0.7, 22.4, 0.1)
+ax2.errorbar(
+    tempo2, f(tempo2, *parametri), ls="-", lw=1, c="k", label="curva di approssimazione"
+)
+val1 = round(parametri[0], 2)
+val2 = round(parametri[1], 2)
+val3 = round(parametri[2], 2)
+val4 = round(parametri[3], 2)
+ax2.axhline(0, c="k", lw=0.5)
+ax2.legend(loc="best")
+ax2.text(
+    12,
+    1,
+    rf"$\Theta$ = ${val1:.2f}e^{{{val2:.2f}t}}sin({val3:.2f}t {val4:+.2f})$",
+    fontsize=11,
+    bbox=dict(facecolor="white", edgecolor="black", lw=0.5),
+)
+mp.show()
---
+++ pendolo_smorzato/crit_smorzato.py
@@ -0,0 +1,89 @@
+import numpy as np
+from scipy import optimize, signal
+import matplotlib.pyplot as mp
+from matplotlib.ticker import MultipleLocator, AutoMinorLocator
+
+corrente, lambda1 = np.loadtxt("./lambda.csv", delimiter=";", unpack=True, skiprows=1)
+
+# grafico senza tendenza
+fig, ax = mp.subplots()
+ax.set_xlabel("CORRENTE (A)", fontsize=11)
+ax.set_ylabel("LAMBDA", fontsize=11)
+ticklabel = ["0", "0.2", "0.4", "0.6", "0.8"]
+yticklist = np.arange(0, 2, 0.5)
+yticklabel = ["0.50", "1.00", "1.50", "2.00"]
+ticklist = np.arange(0, 1, 0.2)
+ax.set_xticks(ticklist)
+ax.set_xticklabels(ticklabel)
+ax.set_yticks(yticklist)
+ax.set_yticklabels(yticklabel)
+ax.yaxis.set_minor_locator(MultipleLocator(0.25))
+ax.xaxis.set_minor_locator(MultipleLocator(0.1))
+ax.grid(which="major", axis="y", linewidth=1, alpha=0.7)
+ax.grid(which="minor", axis="y", linewidth=0.5, alpha=0.5)
+ax.grid(which="major", axis="x", linewidth=1, alpha=0.7)
+ax.grid(which="minor", axis="x", linewidth=0.5, alpha=0.5)
+ax.errorbar(corrente, lambda1, ls="none", marker="o", mfc="b", ms=5, mec="b")
+
+
+def f(x, A, B, C):
+    return A * x**2 + B * x + C
+
+
+parametri, covar = optimize.curve_fit(f, corrente, lambda1)
+print(f"A = {parametri[0]}\nB = {parametri[1]}\nC = {parametri[2]}")
+# grafico con tendenza
+fig2, ax2 = mp.subplots()
+ax2.set_title(r"relazione I - $\lambda$", fontsize=16, pad=15)
+ax2.set_xlabel("CORRENTE (A)", fontsize=11)
+ax2.set_ylabel("LAMBDA", fontsize=11)
+ax2.set_xticks(ticklist)
+ax2.set_xticklabels(ticklabel)
+ax2.set_yticks(yticklist)
+ax2.set_yticklabels(yticklabel)
+ax2.yaxis.set_minor_locator(MultipleLocator(0.25))
+ax2.xaxis.set_minor_locator(MultipleLocator(0.1))
+ax2.grid(which="major", axis="y", linewidth=1, alpha=0.7)
+ax2.grid(which="minor", axis="y", linewidth=0.5, alpha=0.5)
+ax2.grid(which="major", axis="x", linewidth=1, alpha=0.7)
+ax2.grid(which="minor", axis="x", linewidth=0.5, alpha=0.5)
+ax2.errorbar(
+    corrente,
+    lambda1,
+    ls="none",
+    marker="o",
+    mfc="r",
+    ms=3,
+    mec="r",
+    label="dati sperimentali",
+)
+corrente2 = np.arange(0, 1.01, 0.05)
+ax2.errorbar(
+    corrente2,
+    f(corrente2, *parametri),
+    ls="-",
+    lw="1",
+    c="b",
+    marker="none",
+    label="curva di approssimazione",
+)
+A1 = round(parametri[0], 2)
+B1 = round(parametri[1], 2)
+C1 = round(parametri[2], 3)
+ax2.legend(loc="best")
+ax2.text(
+    0.1,
+    1,
+    rf"$\lambda$ = ${A1:.2f}x^2 {B1:+.2f}x {C1:+.2f}$",
+    fontsize=11,
+    bbox=dict(facecolor="white", edgecolor="black", lw=0.5),
+)
+mp.show()
+w0 = input("inserisci w0 smorzamento critico: ")
+C12 = C1 - float(w0)
+coefficienti = [A1, B1, C12]
+radici = np.roots(coefficienti)
+for n in radici:
+    if n >= 0:
+        n = round(n, 3)
+        print(f"I = {n:.2f}")
